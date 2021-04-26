a739a57665ec3fff9d4e05f44905c320c2d36c11
armankhalili <armankhalili7@gmail.com>
2021-04-26 12:11:09 +0430

    successfully

---
+++ bubbleSort_edited.py
@@ -0,0 +1,10 @@
+def buble_sort(list):
+    end = len(list)
+    while end != 0 :
+        for i in range(0,end-1):
+            if list[i]>list[i+1] :
+                list[i] , list[i+1] = list[i+1] , list[i]
+        end -= 1        
+list = [121,24654,6566,786,47,369,48,647]
+buble_sort(list)
+print(list)
