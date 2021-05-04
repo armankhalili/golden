7c63e55e32a8b6606448ce1f6f2528f739b9f563
armankhalili <armankhalili7@gmail.com>
2021-05-04 18:55:46 +0430

    Update files.py

--- files.py
+++ files.py
@@ -1,34 +1,35 @@
-# path = 'E:/Education/CWNA.txt'
-# myfile = open(path, 'r',encoding='utf-8')
+path = 'E:/Education/CWNA.txt'
+myfile = open(path, 'r',encoding='utf-8')
 
 
                    ### read line
 
-# Lines = myfile.readlines()
+Lines = myfile.readlines()
 
-# print ("\n MY new list line : ", Lines ,"\n number of lines of my file : ", len(Lines) )
+print ("\n MY new list line : ", Lines ,"\n number of lines of my file : ", len(Lines) )
 
                    ## write file 
 
-# path2 = 'E:/Education/file.txt'
-# myfile = open(path2, 'w+',encoding='utf-8')
+path2 = 'E:/Education/file.txt'
+myfile = open(path2, 'w+',encoding='utf-8')
 
-# message = "firt writte python string to a file"
+message = "firt writte python string to a file"
 
-# myfile.write(message)
+myfile.write(message)
 
                     ### append string to a file 
 
-# path3 = 'E:/Education/file.txt'
-# myfile = open(path3, 'a',encoding='utf-8')   
+path3 = 'E:/Education/file.txt'
+myfile = open(path3, 'a',encoding='utf-8')   
 
-# for i in range(1,100) :
-#     string = i
-#     if i < 10 :
-#         string = "0{}".format(i)
-#     text = "\n {} == line of file ".format(string)
-#     myfile.write(text)
+for i in range(1,100) :
+    string = i
+    if i < 10 :
+        string = "0{}".format(i)
+    text = "\n {} == line of file ".format(string)
+    myfile.write(text)
 
+                      ### file exist 
 import os 
 
 path3 = 'E:/Education/file3.txt'
