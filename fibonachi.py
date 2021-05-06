ffd6466e5e1fdc03b78f9fdc36d328d8ea871ff6
armankhalili <armankhalili7@gmail.com>
2021-05-06 10:10:35 +0430

    Delete fibonachi.py

--- fibonachi.py
+++
@@ -1,9 +0,0 @@
-def fibo (number):
-    if number == 0 or number == 1 :
-        return number
-    else :
-        return fibo(number-1) + fibo(number-2)
-
-variable =int(input("please Enter your number : ")) 
-for i in range(0,variable) :
-    print(fibo(i)) 
