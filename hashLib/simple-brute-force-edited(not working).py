16d8eca6b1d6508e0bdb0378ca059582cba5f35c
armankhalili <armankhalili7@gmail.com>
2021-05-06 18:41:08 +0430

    Update simple-brute-force-edited(not working).py

--- hashLib/simple-brute-force-edited(not working).py
+++ hashLib/simple-brute-force-edited(not working).py
@@ -1,7 +1,7 @@
 import hashlib
 
 # functions 
-def hash256(password) :
+def hash256(password) :                      #hash the generated password
     hashed = hashlib.sha256(password.encode('utf-8')).hexdigest()
     return hashed
 
@@ -9,10 +9,10 @@ def hash256(password) :
 file = open ("E:/Python code/github/golden/hashLib/pass.csv")
 
 # main 
-for num in range(0,10000) :
-    for line in file :
-        tempPass = str.format("%04d"%num)   # 4 digit generated password
-        userData = line.split(",")      # name and hashed password from file
+for num in range(0,10000) :                  # gereneting password
+    for line in file :                       # read line form file
+        tempPass = str.format("%04d"%num)    # 4 digit form generated password
+        userData = line.split(",")           # name and hashed password from file
 
         if(hash256(tempPass) == userData[1].rstrip()) :
             print("pass of {} is {}".format(userData[0],tempPass))
